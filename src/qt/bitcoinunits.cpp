d5ba6b7a8410e6c5defbb35bc7df278f4bb0943f
MacLane Wilkison <mwilkison@gmail.com>
2014-11-18 10:40:23 -0800

    s/SMC/MAC

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("MAC");
+    case mBTC: return QString("mMAC");
+    case uBTC: return QString::fromUtf8("μMAC");
     default: return QString("???");
     }
 }
